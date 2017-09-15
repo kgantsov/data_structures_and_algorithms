d74ff5ddefe2444cc1f188a9db34385f61d42b6b
Kostiantyn Hantsov <k.gantsov@gmail.com>
2017-09-15 20:08:51 +0200

    Delete python code after a split of the repo into a two separate repos for python and for golang

--- python/sorting/bubble.py
+++
@@ -1,7 +0,0 @@
-
-def sort(lst):
-    for i in range(0, len(lst) - 1):
-        for j in range(1, len(lst) - i):
-            if lst[j - 1] > lst[j]:
-                lst[j - 1], lst[j] = lst[j], lst[j - 1]
-    return lst
