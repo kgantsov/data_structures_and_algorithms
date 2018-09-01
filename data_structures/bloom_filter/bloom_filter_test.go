d75d538a205b5eb9164d37e8bf0acf5e30d3b980
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-09-01 19:17:36 +0200

    Added some tests for BloomFilter

---
+++ data_structures/bloom_filter/bloom_filter_test.go
@@ -0,0 +1,42 @@
+package bloom_filter
+
+import (
+	"testing"
+)
+
+func assetEqual(t *testing.T, expected, actual interface{}) {
+	if expected != actual {
+		t.Errorf("Expected `%v`. Got `%v`\n", expected, actual)
+	}
+}
+
+func TestNewBloomFilter(t *testing.T) {
+	b := NewBloomFilter(0.001, 10)
+
+	assetEqual(t, false, b.Check("user:35"))
+	assetEqual(t, false, b.Check("user:165"))
+	assetEqual(t, false, b.Check("user:55"))
+	assetEqual(t, false, b.Check("user:99"))
+
+	b.Add("user:35")
+
+	assetEqual(t, true, b.Check("user:35"))
+	assetEqual(t, false, b.Check("user:165"))
+	assetEqual(t, false, b.Check("user:55"))
+	assetEqual(t, false, b.Check("user:99"))
+
+	b.Add("user:165")
+
+	assetEqual(t, true, b.Check("user:35"))
+	assetEqual(t, true, b.Check("user:165"))
+	assetEqual(t, false, b.Check("user:55"))
+	assetEqual(t, false, b.Check("user:99"))
+
+	b.Add("user:55")
+	b.Add("user:99")
+
+	assetEqual(t, true, b.Check("user:35"))
+	assetEqual(t, true, b.Check("user:165"))
+	assetEqual(t, true, b.Check("user:55"))
+	assetEqual(t, true, b.Check("user:99"))
+}
